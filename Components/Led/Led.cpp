af32b15b99c0af6b361b53e72e606dfd966480a7
Tirth Thakkar <tirth1226@gmail.com>
2026-06-08 17:13:04 -0700

    Starting LEDs

---
+++ Components/Led/Led.cpp
@@ -0,0 +1,28 @@
+// ======================================================================
+// \title  Led.cpp
+// \author thakkar
+// \brief  cpp file for Led component implementation class
+// ======================================================================
+
+#include "Components/Led/Led.hpp"
+
+namespace ReferenceDeployment {
+
+// ----------------------------------------------------------------------
+// Component construction and destruction
+// ----------------------------------------------------------------------
+
+Led ::Led(const char* const compName) : LedComponentBase(compName) {}
+
+Led ::~Led() {}
+
+// ----------------------------------------------------------------------
+// Handler implementations for commands
+// ----------------------------------------------------------------------
+
+void Led ::TODO_cmdHandler(FwOpcodeType opCode, U32 cmdSeq) {
+    // TODO
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+}
+
+}  // namespace ReferenceDeployment
---
+++ Components/Led/docs/sdd.md
@@ -0,0 +1,21 @@
+# ReferenceDeployment::Led
+
+Component to blink LEDs on Nucleo STM32H7
+
+## Introduction
+
+<!-- High level introduction, FPP interfaces?, high-level features? -->
+
+## Requirements
+
+| Name | Description | Rationale | Validation |
+|---|---|---|---|
+|   |   |   |   |
+
+## Design
+
+<!-- Explain high level design and important internal details -->
+
+## Configuration
+
+<!-- If the component requires configuration at initialization, document here -->
